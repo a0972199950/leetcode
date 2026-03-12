e8ec0b222bdc468fa98f937ce1a7df1af8353dbb
john.hsieh@bytedance.com <john.hsieh@bytedance.com>
2026-03-12 12:37:40 +0800

    .

--- problems/636.ts
+++ problems/636.ts
@@ -1,7 +1,7 @@
 // 636. Exclusive Time of Functions
-// https://leetcode.com/problems/exclusive-time-of-functions/description/?envType=problem-list-v2&envId=dsa-linear-shoal-stack
 
 export {}
+
 console.clear()
 
 // type Status = 'start' | 'end'
@@ -42,6 +42,7 @@ console.clear()
 // Time: O(n)
 // Space: O(n)
 function exclusiveTime(n: number, logs: string[]): number[] {
+
   const parseLog = (log: string) => {
     const [fn, type, timestamp] = log.split(':')
 
@@ -56,6 +57,7 @@ function exclusiveTime(n: number, logs: string[]): number[] {
   const result = Array(n).fill(0)
   let lastTimestamp = 0
 
+  let lastTimestamp = 0
   for (const log of logs) {
     const { fn, type, timestamp } = parseLog(log)
 
@@ -81,3 +83,6 @@ function exclusiveTime(n: number, logs: string[]): number[] {
 console.log(exclusiveTime(2, ['0:start:0', '1:start:2', '1:end:5', '0:end:6']))
 console.log(exclusiveTime(1, ['0:start:0', '0:start:2', '0:end:5', '0:start:6', '0:end:6', '0:end:7']))
 console.log(exclusiveTime(2, ['0:start:0', '0:start:2', '0:end:5', '1:start:6', '1:end:6', '0:end:7']))
+console.log(exclusiveTime(8, ['0:start:0', '1:start:5', '2:start:6', '3:start:9', '4:start:11', '5:start:12', '6:start:14', '7:start:15', '1:start:24', '1:end:29', '7:end:34', '6:end:37', '5:end:39', '4:end:40', '3:end:45', '0:start:49', '0:end:54', '5:start:55', '5:end:59', '4:start:63', '4:end:66', '2:start:69', '2:end:70', '2:start:74', '6:start:78', '0:start:79', '0:end:80', '6:end:85', '1:start:89', '1:end:93', '2:end:96', '2:end:100', '1:end:102', '2:start:105', '2:end:109', '0:end:114']))
+console.log(exclusiveTime(2, ['0:start:0', '1:start:2', '1:end:3', '2:start:5', '2:end:6', '0:end:8']))
+
