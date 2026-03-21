d975b0f62ccd043d3ebb6bd17197c6696a951ada
John H <a0972199950@gmail.com>
2026-03-21 21:26:00 +0800

    567

--- problems/567.ts
+++ problems/567.ts
@@ -3,103 +3,150 @@
 export {}
 console.clear()
 
-class SlideWindow {
-  leftIndex: number | null = null
-  rightIndex: number | null = null
-  length: number
+// class SlideWindow {
+//   leftIndex: number | null = null
+//   rightIndex: number | null = null
+//   length: number
   
-  constructor (length: number) {
-    this.length = length
-  }
+//   constructor (length: number) {
+//     this.length = length
+//   }
 
-  set (index: number) {
-    this.leftIndex = this.rightIndex = index
-  }
+//   set (index: number) {
+//     this.leftIndex = this.rightIndex = index
+//   }
 
-  reset () {
-    this.leftIndex = this.rightIndex = null
-  }
+//   reset () {
+//     this.leftIndex = this.rightIndex = null
+//   }
 
-  increase () {
-    this.rightIndex++
-  }
+//   increase () {
+//     this.rightIndex++
+//   }
 
-  decrease () {
-    this.leftIndex++
-  }
+//   decrease () {
+//     this.leftIndex++
+//   }
 
-  isFound () {
-    if (this.leftIndex === null || this.rightIndex === null) {
-      return false
-    }
-    return this.rightIndex - this.leftIndex + 1 === this.length
-  }
-}
+//   isFound () {
+//     if (this.leftIndex === null || this.rightIndex === null) {
+//       return false
+//     }
+//     return this.rightIndex - this.leftIndex + 1 === this.length
+//   }
+// }
 
-class CharTable {
-  data: Record<string, number> = {}
+// class CharTable {
+//   data: Record<string, number> = {}
 
-  constructor (s: string) {
-    for (const char of s.split('')) {
-      this.data[char] = ++this.data[char] || 1
-    }
-  }
+//   constructor (s: string) {
+//     for (const char of s.split('')) {
+//       this.data[char] = ++this.data[char] || 1
+//     }
+//   }
 
-  add (char: string) {
-    this.data[char]++
-  }
+//   add (char: string) {
+//     this.data[char]++
+//   }
 
-  remove (char: string) {
-    this.data[char]--
-  }
-}
+//   remove (char: string) {
+//     this.data[char]--
+//   }
+// }
 
-function checkInclusion(s1: string, s2: string): boolean {
-  const charTable = new CharTable(s1)
-  const slideWindow = new SlideWindow(s1.length)
+// function checkInclusion(s1: string, s2: string): boolean {
+//   const charTable = new CharTable(s1)
+//   const slideWindow = new SlideWindow(s1.length)
 
-  let index = 0
+//   let index = 0
 
+//   const handleHit = () => {
+//     charTable.remove(s2[index])
 
-  const handleHit = () => {
-    charTable.remove(s2[index])
-
-    if (slideWindow.leftIndex === null) {
-      slideWindow.set(index)
-    }
-    else {
-      slideWindow.increase()
-    }
+//     if (slideWindow.leftIndex === null) {
+//       slideWindow.set(index)
+//     }
+//     else {
+//       slideWindow.increase()
+//     }
     
-    index++
+//     index++
+//   }
+
+//   const handleMiss = () => {
+//     if (slideWindow.leftIndex === null && slideWindow.rightIndex === null) {
+//       index++
+//     }
+//     else if (slideWindow.leftIndex === slideWindow.rightIndex) {
+//       charTable.add(s2[slideWindow.leftIndex])
+//       slideWindow.reset()
+//       index++
+//     }
+//     else {
+//       charTable.add(s2[slideWindow.leftIndex])
+//       slideWindow.decrease()
+//     }
+//   }
+
+//   while (index < s2.length) {
+//     const char = s2[index]
+
+//     if (charTable.data[char]) {
+//       handleHit()
+//     }
+//     else {
+//       handleMiss()
+//     }
+
+//     if (slideWindow.isFound()) {
+//       return true
+//     }
+//   }
+
+//   return false
+// }
+
+// TODO: [3/21] 沒自己解出來，待再次練習
+// Time O(n)
+// Space O(1)
+function checkInclusion(s1: string, s2: string): boolean {
+  if (s2.length < s1.length) {
+    return false
   }
 
-  const handleMiss = () => {
-    if (slideWindow.leftIndex === null && slideWindow.rightIndex === null) {
-      index++
-    }
-    else if (slideWindow.leftIndex === slideWindow.rightIndex) {
-      charTable.add(s2[slideWindow.leftIndex])
-      slideWindow.reset()
-      index++
-    }
-    else {
-      charTable.add(s2[slideWindow.leftIndex])
-      slideWindow.decrease()
-    }
+  const map: Record<string, number> = {}
+  let matches = 0
+
+  for (const s of s1) {
+    map[s] = (map[s] ?? 0) + 1
   }
 
-  while (index < s2.length) {
-    const char = s2[index]
+  // console.log(map)
 
-    if (charTable.data[char]) {
-      handleHit()
-    }
-    else {
-      handleMiss()
+  let left = 0
+  let right = 0
+
+  for (right = 0; right < s2.length; right++) {
+    const rightChar = s2[right]
+
+    map[rightChar] = (map[rightChar] ?? 0) - 1
+
+    if (map[rightChar] >= 0) {
+      matches++
     }
 
-    if (slideWindow.isFound()) {
+    if (right - left + 1 > s1.length) {
+      const leftChar = s2[left]
+      map[leftChar] = (map[leftChar] ?? 0) + 1
+
+      if (map[leftChar] > 0) {
+        matches--
+      }
+
+      left++
+    }
+
+    if (matches === s1.length) {
       return true
     }
   }
@@ -110,5 +157,5 @@ function checkInclusion(s1: string, s2: string): boolean {
 console.log(checkInclusion('ab', 'eidbaooo')) // true
 console.log(checkInclusion('ab', 'eidboaoo')) // false
 console.log(checkInclusion('adc', 'dcda')) // true
-
+console.log(checkInclusion('hello', 'ooolleoooleh')) // false
 
