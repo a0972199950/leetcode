560160ca5bb96a475e3d8c773e68f984d901959a
John H <a0972199950@gmail.com>
2026-04-03 12:52:25 +0800

    12

--- problems/12.ts
+++ problems/12.ts
@@ -4,48 +4,123 @@
 export {}
 console.clear()
 
+// function intToRoman(num: number): string {
+//   const romans: [string, number][] = [
+//     ['M', 1000], ['CM', 900],
+//     ['D', 500],  ['CD', 400],
+//     ['C', 100],  ['XC', 90],
+//     ['L', 50],   ['XL', 40],
+//     ['X', 10],   ['IX', 9],
+//     ['V', 5],    ['IV', 4],
+//     ['I', 1]
+//   ]
+
+//   let result = ''
+
+//   for (const sub of [1000, 100, 10, 1]) {
+//     let remain = Math.floor(num / sub) * sub
+//     num %= sub
+
+//     if (!remain) {
+//       continue
+//     }
+
+//     for (const [symbol, value] of romans) {
+//       const count = Math.floor(remain / value)
+
+//       if (!count) {
+//         continue
+//       }
+
+//       result += Array(count).fill(symbol).join('')
+
+//       remain %= value
+//       if (!remain) {
+//         break
+//       }
+//     }
+//   }
+
+//   return result
+// }
+
+// Time: O(1)
+// Space: O(1)
+function intToRoman(_num: number): string {
+  const s = String(_num)
+  let result = ''
+
+  const map = {
+    [1000]: 'M',
+
+    [500]: 'D',
+    [900]: 'CM',
+    [400]: 'CD',
+    [100]: 'C',
+
+    [50]: 'L',
+    [90]: 'XC',
+    [40]: 'XL',
+    [10]: 'X',
+
+    [5]: 'V',
+    [9]: 'IX',
+    [4]: 'IV',
+    [1]: 'I'
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    const num = Number(s[i]) * (10 ** (s.length - i - 1))
+    // console.log('num:', num)
+
+    if (map[num]) {
+      result += map[num]
+      continue
+    }
+
+    const j = 10 ** (s.length - i - 1)
+    const divideResult = num / j
+  
+    if (num < 5 * j) {
+      result += map[j].repeat(divideResult)
+    } else {
+      result += (map[5 * j] + map[j].repeat(divideResult - 5))
+    }
+
+  }
+
+  return result
+}
+
+// AI 寫的，不是我想到的
+// Time: O(1)
+// Space: O(1)
 function intToRoman(num: number): string {
-  const romans: [string, number][] = [
-    ['M', 1000], ['CM', 900],
-    ['D', 500],  ['CD', 400],
-    ['C', 100],  ['XC', 90],
-    ['L', 50],   ['XL', 40],
-    ['X', 10],   ['IX', 9],
-    ['V', 5],    ['IV', 4],
-    ['I', 1]
+  const romans: [number, string][] = [
+    [1000, 'M'], [900, 'CM'],
+    [500, 'D'],  [400, 'CD'],
+    [100, 'C'],  [90, 'XC'],
+    [50, 'L'],   [40, 'XL'],
+    [10, 'X'],   [9, 'IX'],
+    [5, 'V'],    [4, 'IV'],
+    [1, 'I']
   ]
 
   let result = ''
 
-  for (const sub of [1000, 100, 10, 1]) {
-    let remain = Math.floor(num / sub) * sub
-    num %= sub
-
-    if (!remain) {
-      continue
-    }
-
-    for (const [symbol, value] of romans) {
-      const count = Math.floor(remain / value)
-
-      if (!count) {
-        continue
-      }
-
-      result += Array(count).fill(symbol).join('')
-
-      remain %= value
-      if (!remain) {
-        break
-      }
+  for (const [value, symbol] of romans) {
+    while (num >= value) {
+      result += symbol
+      num -= value
     }
   }
 
   return result
 }
 
-// console.log(intToRoman(9))
-// console.log(intToRoman(13))
+console.log(intToRoman(9))
+console.log(intToRoman(13))
 console.log(intToRoman(3749))
-
+console.log(intToRoman(58))
+console.log(intToRoman(1994))
 
--- problems/763.ts
+++ problems/763.ts
@@ -10,45 +10,107 @@ interface Part {
   end: number
 }
 
+// function partitionLabels(s: string): number[] {
+//   const parts: Part[] = []
+
+//   s_loop:
+//   for (let sIndex = 0; sIndex < s.length; sIndex++) {
+//     const char = s[sIndex]
+
+//     for (let pIndex = 0; pIndex < parts.length; pIndex++) {
+//       const part = parts[pIndex]
+
+//       if (part.chars.has(char)) {
+//         const rest = parts.splice(pIndex + 1)
+
+//         part.chars = new Set<string>([
+//           ...part.chars,
+//           ...rest.reduce((sum, { chars }) => [...sum, ...chars], [])
+//         ])
+
+//         part.end = sIndex
+//         continue s_loop
+//       }
+//     }
+
+//     parts.push({
+//       chars: new Set<string>().add(char),
+//       start: sIndex,
+//       end: sIndex
+//     })
+//   }
+
+//   return parts.map(({ start, end }) => end - start + 1)
+// }
+
+// Time: O(n)
+// Space: O(n)
+// function partitionLabels(s: string): number[] {
+//   // { char: stack 第幾個位置 }
+//   const record: Map<string, number> = new Map()
+//   const stack: number[] = []
+
+//   for (const char of s) {
+//     if (!record.has(char)) {
+//       record.set(char, stack.length)
+//       stack.push(1)
+//       continue
+//     }
+
+//     let sum = 1
+
+//     for (let i = stack.length - 1; i >= record.get(char); i--) {
+//       sum += stack.pop()
+//     }
+
+//     // Time: O(1): 最多 26 個字母
+//     record.forEach((value, key) => {
+//       if (value >= record.get(char)) {
+//         record.set(key, record.get(char))
+//       }
+//     })
+//     // console.log(char, 'toBeMarged: ', toBeMarged)
+//     stack.push(sum)
+//   }
+
+//   // console.log(stack)
+//   return stack
+// }
+
+// Time: O(n)
+// Space: O(1)
 function partitionLabels(s: string): number[] {
-  const parts: Part[] = []
-
-  s_loop:
-  for (let sIndex = 0; sIndex < s.length; sIndex++) {
-    const char = s[sIndex]
-
-    for (let pIndex = 0; pIndex < parts.length; pIndex++) {
-      const part = parts[pIndex]
-
-      if (part.chars.has(char)) {
-        const rest = parts.splice(pIndex + 1)
-
-        part.chars = new Set<string>([
-          ...part.chars,
-          ...rest.reduce((sum, { chars }) => [...sum, ...chars], [])
-        ])
-
-        part.end = sIndex
-        continue s_loop
-      }
-    }
-
-    parts.push({
-      chars: new Set<string>().add(char),
-      start: sIndex,
-      end: sIndex
-    })
+  // { char: 字元最後出現的位置 }
+  const record: Map<string, number> = new Map()
+  for (let i = 0; i < s.length; i++) {
+    record.set(s[i], i)
   }
 
-  return parts.map(({ start, end }) => end - start + 1)
+  // console.log(record)
+  
+  let start = 0
+  let end = 0
+
+  const result = []
+  
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i]
+
+    end = Math.max(end, record.get(char))
+
+    if (i === end) {
+      result.push(end - start + 1)
+      start = i + 1
+    }
+    
+  }
+
+  return result
 }
 
-// partitionLabels('abcad')
-// console.log(partitionLabels('ababcbacadefegdehijhklij'))
-// console.log(partitionLabels('eccbbbbdec'))
+console.log(partitionLabels('ababcc'))
+console.log(partitionLabels('ababcbacadefegdehijhklij'))
+console.log(partitionLabels('eccbbbbdec'))
 console.log(partitionLabels('abcd'))
 console.log(partitionLabels('aaaa'))
-console.log(partitionLabels(''))
-
-
 
