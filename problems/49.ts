4af95e0e98e29d58ed85d58da3560ef69d1fefa5
John H <a0972199950@gmail.com>
2026-05-02 13:24:11 +0800

    49

--- problems/49.ts
+++ problems/49.ts
@@ -4,32 +4,60 @@
 export {}
 console.clear()
 
+// function groupAnagrams(strs: string[]): string[][] {
+//   const results: Record<string, string[]> = {}
+
+//   const charCodePrefix = 'a'.charCodeAt(0)
+
+//   const generateStrKey = (str: string) => {
+//     const arr = Array(26).fill(0)
+
+//     for (let i = 0; i < str.length; i++) {
+//       arr[str[i].charCodeAt(0) - charCodePrefix]++
+//     }
+
+//     return arr.join(',')
+//   }
+
+//   for (const str of strs) {
+//     const key = generateStrKey(str)
+
+//     if (!results[key]) {
+//       results[key] = [str]
+//     } else {
+//       results[key].push(str)
+//     }
+//   }
+
+//   return Object.values(results)
+// }
+
+// Time: O(n * k)
+// Space: O(n)
 function groupAnagrams(strs: string[]): string[][] {
-  const results: Record<string, string[]> = {}
+  const getHashKey = (s: string) => {
+    const result = Array(26).fill(0)
 
-  const charCodePrefix = 'a'.charCodeAt(0)
-
-  const generateStrKey = (str: string) => {
-    const arr = Array(26).fill(0)
-
-    for (let i = 0; i < str.length; i++) {
-      arr[str[i].charCodeAt(0) - charCodePrefix]++
+    for (const c of s) {
+      const charCode = c.charCodeAt(0) - 'a'.charCodeAt(0)
+      result[charCode]++
     }
 
-    return arr.join(',')
+    return result.join(',')
   }
+
+  const hashTable: Record<string, string[]> = {}
 
   for (const str of strs) {
-    const key = generateStrKey(str)
-
-    if (!results[key]) {
-      results[key] = [str]
+    const hashKey = getHashKey(str)
+    if (hashTable[hashKey]) {
+      hashTable[hashKey].push(str)
     } else {
-      results[key].push(str)
+      hashTable[hashKey] = [str]
     }
   }
 
-  return Object.values(results)
+  return Object.values(hashTable)
 }
 
 console.log(groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']))
