fc9b1dd1d7057a80a84fbc22652d709f818821bd
John H <a0972199950@gmail.com>
2026-09-18 10:50:18 +0800

    update heap

--- data-structure/Heap.ts
+++ data-structure/Heap.ts
@@ -1,11 +1,11 @@
 
 console.clear()
 
-export class MinHeap {
-  data = []
-  getVal: (node: any) => number
+export class MinHeap<T = number> {
+  data: T[] = []
+  getVal: (node: T) => number
 
-  constructor (getVal: (node: any) => number = (val: number) => val) {
+  constructor (getVal: (node: T) => number = (val: any) => val) {
     this.getVal = getVal
   }
 
@@ -13,7 +13,7 @@ export class MinHeap {
     return this.data.length
   }
 
-  enqueue (node?: any) {
+  enqueue (node?: T) {
     if (node === undefined) {
       return
     }
@@ -67,7 +67,73 @@ export class MinHeap {
   }
 }
 
-const heap = new MinHeap((node) => node.value)
+export class MaxHeap<T = number> {
+  data: T[] = []
+  getVal: (node: T) => number
+
+  constructor (getVal: (node: T) => number = (val: any) => val) {
+    this.getVal = getVal
+  }
+
+  get size () {
+    return this.data.length
+  }
+
+  enqueue (node?: T) {
+    if (node === undefined) {
+      return
+    }
+
+    this.data.push(node)
+
+    let index = this.data.length - 1
+
+    while (index > 0) {
+      const parentIndex = Math.floor((index - 1) / 2)
+
+      if (this.getVal(node) >= this.getVal(this.data[parentIndex])) {
+        [this.data[index], this.data[parentIndex]] = [this.data[parentIndex], this.data[index]]
+        index = parentIndex
+      } else {
+        break
+      }
+    }
+  }
+
+  dequeue () {
+    const node = this.data.shift()
+
+    if (this.data.length) {
+      this.data.unshift(this.data.pop())
+      let currentIndex = 0
+
+      while (currentIndex < this.data.length) {
+        const leftIndex = currentIndex * 2 + 1
+        const rightIndex = currentIndex * 2 + 2
+        let maxIndex = currentIndex
+
+        if (leftIndex < this.data.length && this.getVal(this.data[leftIndex]) > this.getVal(this.data[maxIndex])) {
+          maxIndex = leftIndex
+        }
+
+        if (rightIndex < this.data.length && this.getVal(this.data[rightIndex]) > this.getVal(this.data[maxIndex])) {
+          maxIndex = rightIndex
+        }
+
+        if (maxIndex === currentIndex) {
+          break
+        }
+
+        [this.data[maxIndex], this.data[currentIndex]] = [this.data[currentIndex], this.data[maxIndex]]
+        currentIndex = maxIndex
+      }
+    }
+
+    return node
+  }
+}
+
+const heap = new MinHeap<{ key: string, value: number }>((node) => node.value)
 
 heap.enqueue({ key: 'a', value: 41 })
 heap.enqueue({ key: 'a', value: 39 })
---
+++ problems/arta/atra.ts
@@ -0,0 +1,12 @@
+import { } from '~/data-structure/BinaryTree'
+import { } from '~/data-structure/Heap'
+
+console.clear()
+
+function fn(): any {
+
+}
+
+console.log(fn()) // 2
+console.log(fn()) // 3
+console.log(fn()) // 0
